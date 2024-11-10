a5539afc731844e8bb2f44066ebc07e901ee143f
hoken92 <hkenneth92@gmail.com>
2024-11-10 14:10:57 -0800

    added article html element

--- components/Article.jsx
+++ components/Article.jsx
@@ -5,11 +5,11 @@ import ArticleImg from "./ArticleImg";
 function Article({ article }) {
   const { title, date, img, alt, content } = article;
   return (
-    <>
+    <article>
       <ArticleTitle title={title} date={date} />
       <ArticleImg img={img} alt={alt} />
       <ArticleBody content={content} />
-    </>
+    </article>
   );
 }
 
